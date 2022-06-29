708a21925dc0136abc01d15b6ea4eb9497a24f9c
Joshua Stresing <pyrotorch69@yahoo.com>
2022-06-28 17:09:18 -0700

    got my users stuck working and checked out

---
+++ __tests__/users.test.js
@@ -0,0 +1,52 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UsersService');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  const mockUser = {
+    email: 'test@example.com',
+    password: '123456',
+  };
+
+  const registerAndLogin = async (userProps = {}) => {
+    const password = userProps.password ?? mockUser.password;
+
+    const agent = request.agent(app);
+
+    const user = await UserService.create({ ...mockUser, ...userProps });
+
+    const { email } = user;
+    await agent.post('/api/v1/users/sessions').send({ email, password });
+    return [agent, user];
+  };
+
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email,
+    });
+  });
+
+  it('returns the current user', async () => {
+    const [agent, user] = await registerAndLogin();
+    const me = await agent.get('/api/v1/users/me');
+    expect(me.body).toEqual({
+      ...user,
+      exp: expect.any(Number),
+      iat: expect.any(Number),
+    });
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
