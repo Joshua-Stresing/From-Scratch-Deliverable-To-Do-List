3d697863f834a887aea89cb8ee9cd06b19ab1b9a
Joshua Stresing <pyrotorch69@yahoo.com>
2022-06-29 08:29:50 -0700

    forgot to commit, need to figure out whats going on wit tests

---
+++ __tests__/todos.test.js
@@ -0,0 +1,43 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const mockUser = {
+  email: 'test@example.com',
+  password: '12345678',
+};
+
+const LoggedIn = async (props = {}) => {
+  const password = props.password ?? mockUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...mockUser, ...props });
+  const { email } = user;
+
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
+
+describe('todo test suite', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('Test to create new todo', async () => {
+    const [agent] = await LoggedIn();
+    const todo = { description: 'Finish APP' };
+    const data = await agent.post('/api/v1/todos/').send(todo);
+    expect(data.status).toEqual(200);
+    expect(data.body).toEqual({
+      id: expect.any(String),
+      user_id: expect.any(String),
+      description: 'Finish APP',
+      completed: false,
+    });
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,6 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS todos;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -9,7 +9,7 @@ CREATE TABLE users (
   password_hash VARCHAR NOT NULL
 );
 
-CREATE TABLE items (
+CREATE TABLE todos (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   user_id BIGINT ,
   description VARCHAR NOT NULL,
